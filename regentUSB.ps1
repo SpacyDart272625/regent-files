e270ef0a93f1d50a71688abf0fb2c9040cb0dc7f
SpacyDart272625 <121508253+SpacyDart272625@users.noreply.github.com>
2024-03-30 16:01:06 -0300

    Update regentUSB.ps1

--- regentUSB.ps1
+++ regentUSB.ps1
@@ -1,24 +1,17 @@
 Clear-Host
 
+# Specify the registry path
 $registryPath = "HKLM:\SYSTEM\MountedDevices"
 
 try {
+    # Retrieve the properties from the registry
     $mountedDevicesInfo = Get-ItemProperty -Path $registryPath -ErrorAction Stop
 
-    $script:Paused = $false
-    $job = Start-Job -ScriptBlock {
-        param($gridView)
-        $gridView | Out-GridView -Title "Regent USB" -PassThru | ForEach-Object {
-            Remove-Job -Job $job
-            $script:Paused = $false
-        }
-    } -ArgumentList ( $mountedDevicesInfo | Select-Object -Property * -ExcludeProperty PS* )
-    $script:Paused = $true
-    while ($script:Paused) {
-        Start-Sleep -Milliseconds 100
-    }
+    # Display only the property names
+    $mountedDevicesInfo | Select-Object -Property * -ExcludeProperty PS* | ForEach-Object { $_.PSObject.Properties.Name } | Out-GridView -Title "Regent USB"
 }
 catch {
     Write-Host "Error accessing registry path: $registryPath" -ForegroundColor Red
     Write-Host "Error Message: $_" -ForegroundColor Red
 }
+pause
