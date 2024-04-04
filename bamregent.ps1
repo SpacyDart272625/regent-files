b593a4505d3de14a9d21891adad32795dff18c76
SpacyDart272625 <121508253+SpacyDart272625@users.noreply.github.com>
2024-04-04 17:21:21 -0300

    Update bam.ps1

--- bamregent.ps1
+++ bamregent.ps1
@@ -1,18 +1,11 @@
-Clear-Host
-
-# Specify the registry path
 $registryPath = "HKLM:\SYSTEM\CurrentControlSet\Services\bam\State\UserSettings\S-1-5-21-*1001"
 
 try {
-    # Retrieve the properties from the registry
-    $mountedDevicesInfo = Get-ItemProperty -Path $registryPath -ErrorAction Stop
-
-    # Display only the property names
-    $mountedDevicesInfo | Select-Object -Property * -ExcludeProperty PS* | ForEach-Object { $_.PSObject.Properties.Name } | Out-GridView -Title "Regent BAM"
+    $bam = Get-ItemProperty -Path $registryPath -ErrorAction Stop
+    $bam | Select-Object -Property * -ExcludeProperty PS* | ForEach-Object { $_.PSObject.Properties.Name } | Out-GridView -Title "Simple bam lol"
 }
 catch {
     Write-Host "Error accessing registry path: $registryPath" -ForegroundColor Red
     Write-Host "Error Message: $_" -ForegroundColor Red
 }
-pause
 
