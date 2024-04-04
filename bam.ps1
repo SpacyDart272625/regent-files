1b0f2a3f727096d575334b46428a49508c1b580a
SpacyDart272625 <121508253+SpacyDart272625@users.noreply.github.com>
2024-04-04 17:25:09 -0300

    Update bam.ps1

--- bam.ps1
+++ bam.ps1
@@ -2,10 +2,20 @@ $registryPath = "HKLM:\SYSTEM\CurrentControlSet\Services\bam\State\UserSettings\
 
 try {
     $bam = Get-ItemProperty -Path $registryPath -ErrorAction Stop
-    $bam | Select-Object -Property * -ExcludeProperty PS* | ForEach-Object { $_.PSObject.Properties.Name } | Out-GridView -Title "Simple bam lol"
+    $bam | Select-Object -Property * -ExcludeProperty PS* | ForEach-Object { $_.PSObject.Properties.Name } | Out-GridView -Title "Simple bam lol" -PassThru -Wait
+    
+    # Loop to continuously check if GridView window is open
+    while ($true) {
+        $gridViewOpen = Get-Process | Where-Object { $_.MainWindowTitle -eq "Simple bam lol" }
+        if ($gridViewOpen) {
+            Start-Sleep -Milliseconds 500  # Sleep for a short duration before checking again
+        }
+        else {
+            break  # Break the loop if GridView window is closed
+        }
+    }
 }
 catch {
     Write-Host "Error accessing registry path: $registryPath" -ForegroundColor Red
     Write-Host "Error Message: $_" -ForegroundColor Red
 }
-
