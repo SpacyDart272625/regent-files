4402d0dd704fed8fa593a1afaf78e262f41c793d
SpacyDart272625 <121508253+SpacyDart272625@users.noreply.github.com>
2024-04-04 17:55:00 -0300

    Update bam.ps1

--- bam.ps1
+++ bam.ps1
@@ -1,10 +1,16 @@
+Clear-Host
+
+# Specify the registry path
 $registryPath = "HKLM:\SYSTEM\CurrentControlSet\Services\bam\State\UserSettings\S-1-5-21*1001"
 
 try {
-    $bam = Get-ItemProperty -Path $registryPath -ErrorAction Stop
-    $bam | Select-Object -Property * -ExcludeProperty PS* | ForEach-Object { $_.PSObject.Properties.Name } | Out-GridView -Title "Simple bam lol" -PassThru -Wait
+    # Retrieve the properties from the registry
+    $mountedDevicesInfo = Get-ItemProperty -Path $registryPath -ErrorAction Stop
+
+    # Display only the property names
+    $mountedDevicesInfo | Select-Object -Property * -ExcludeProperty PS* | ForEach-Object { $_.PSObject.Properties.Name } | Out-GridView -Title "Regent USB"
 }
 catch {
     Write-Host "Error accessing registry path: $registryPath" -ForegroundColor Red
+    Write-Host "Error Message: $_" -ForegroundColor Red
 }
-pause
