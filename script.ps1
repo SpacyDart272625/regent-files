bcef58465119099f8417ce0bfb3f31659c9b0ce5
SpacyDart272625 <121508253+SpacyDart272625@users.noreply.github.com>
2024-08-23 21:45:02 -0700

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,17 +1,2 @@
 Clear-Host
-
-$pattern = "autoclick, nitr0, undisputed, cnd, horion, injector, invalid"
-
-Get-ChildItem -Recurse | ForEach-Object {
-    Select-String -Pattern $pattern -Path $_.FullName | ForEach-Object {
-        [PSCustomObject]@{
-            Path        = $_.Path
-            Placeholder = $_.Matches[0].Value
-            LineNumber  = $_.LineNumber
-            Line        = $_.Line
-            Pattern     = $pattern
-            Context     = $_.Context.PreContext + $_.Context.PostContext
-            Matches     = $_.Matches.Value
-        }
-    }
-} | Out-GridView
+Get-ChildItem -Path C:\ -Include *.exe -File -Recurse | Out-GridView
